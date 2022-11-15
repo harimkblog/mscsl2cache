e5c15c07c771384a356c83d94f41f60011809229
harimkblog <harimkblog@users.noreply.github.com>
2022-11-14 22:18:43 -0500

    cache eviction policy set for ehcache

---
+++ ORMLevel2CacheStudy/src/main/java/upg/mscs/orml2cache/config/CacheConfig.java
@@ -0,0 +1,9 @@
+package upg.mscs.orml2cache.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.cache.annotation.EnableCaching;
+
+@Configuration
+@EnableCaching
+public class CacheConfig {
+}
